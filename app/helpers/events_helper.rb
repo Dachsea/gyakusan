96ae07b9297ccd41a2024ba9c6147e309108c9db
dach <daichi.yoshikawa1007@gmail.com>
2017-10-25 11:47:10 +0900

    イベントの日付関連を追加

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,13 @@
 module EventsHelper
+  def event_dead_changer(event)
+    today = Date.today
+    if event.dead == today
+      text = "今日です"
+    elsif event.dead >= today
+      #一度整数型に変換した後、文字列型に変換(分数で表示されるのを防ぐ)
+      text = "あと：" + (event.dead - today).to_i.to_s + "日"
+    else
+      text = "経過：" + (today - event.dead).to_i.to_s + "日"
+    end
+  end
 end
